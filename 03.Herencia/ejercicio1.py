0acb54bf4d23773090f15b60fe213282d7753dc1
Juan Arguello <arguellojuan08@gmail.com>
2023-08-23 19:37:38 -0300

    herencia

---
+++ 03.Herencia/ejercicio1.py
@@ -0,0 +1,8 @@
+# Ejercicio 2
+# 	Diseñe e implemente las clases Circulo y Cilindro, empleando composición y luego empleando herencia, de manera tal que pueda calcular el área y el volumen respectivamente.
+
+    
+
+class Cilindro:
+    def __init__(self):
+        
